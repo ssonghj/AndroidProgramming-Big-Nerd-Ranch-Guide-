57901c673fef8addff56b9b07570f84f67746907
hyeonjin0103 <hyeonjin0103@o.cnu.ac.kr>
2022-01-20 01:39:55 +0900

    chapter6_makeSecondActivity

---
+++ chapter6/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -0,0 +1,56 @@
+package com.bignerdranch.android.geoquiz
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.security.AccessControlContext
+
+private const val EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true"
+const val EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown"
+
+class CheatActivity : AppCompatActivity() {
+
+    private lateinit var answerTextView : TextView
+    private lateinit var showAnswerButton : Button
+
+    private var answerIsTrue = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false)
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+        showAnswerButton.setOnClickListener{
+            val answerText = when{
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            answerTextView.setText(answerText)
+            setAnswerShownResult(true)
+        }
+    }
+
+    private fun setAnswerShownResult(isAnswerShown: Boolean){
+        val data = Intent().apply {
+            putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        }
+        setResult(Activity.RESULT_OK,data)
+    }
+
+    //동반 객체를 사용하면 클래스 인스턴스 생성하지 않고 동반 객체의 함수를 사용할 수 있음.
+    //동반 객체를 포함하는 클래스 이름을 사용해 동반 객체의 함수를 호출할 수 있음.
+    //동반 객체를 포함하는 클래스에서는 동반 객체를 자신의 멤버인 것처럼 인식하므로 편리.
+    companion object{
+        fun newIntent(packageContext: Context, answerIsTrue:Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
+    }
+}
---
+++ chapter6/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.bignerdranch.android.geoquiz
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+private const val TAG = "MainActivity"
+private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
+
+class MainActivity : AppCompatActivity() {
+    //lateinit : 속성을 사용하기 전에 책임지고 초기화하겠다는 것을 컴파일러에게 알려줌.
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var cheatButton: Button
+    private lateinit var questionTextView: TextView
+
+    //QuizViewModel 인스턴스 보존하기 위해 늦게 초기화되는 속성 추가
+    //by lazy 키워드 사용시 var아닌 val 속성으로 선언 가능
+    //인스턴스 생성시 한번만 초기화되기 때문
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProvider(this).get(QuizViewModel::class.java)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+        setContentView(R.layout.activity_main)
+
+        //Bundle 객체에 저장된 값 확인하고 값 있으면 넣고 없으면 0으로 초기화
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX,0)?:0
+        quizViewModel.currentIndex = currentIndex
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        cheatButton = findViewById(R.id.cheat_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+        trueButton.setOnClickListener{ view: View ->
+            checkAnswer(true)
+        }
+        falseButton.setOnClickListener{ view: View->
+            checkAnswer(false)
+        }
+        //질문 가져와서 화면에 반영
+        updateQuestion()
+
+        cheatButton.setOnClickListener{
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            //어떤 자식 액티비티가 결과를 돌려주는 것인지 알고자 할때도 사용된다.
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+        }
+
+        //질문 총 개수보다 많을 경우 총 개수의 나머지를 활용하여 처음으로 돌아가기, 버튼 클릭시 질문 넘어감
+        nextButton.setOnClickListener{
+            quizViewModel.moveToNext()
+            updateQuestion()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode != Activity.RESULT_OK){
+            return
+        }
+
+        if(requestCode == REQUEST_CODE_CHEAT){
+            quizViewModel.isCheater = data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    //currentIndex 값을 bundle객체에 저장한다.
+    override fun onSaveInstanceState(savedInstanceState: Bundle){
+        super.onSaveInstanceState(savedInstanceState)
+        Log.d(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX,quizViewModel.currentIndex)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    private fun updateQuestion(){
+        val questionTextResId = quizViewModel.currentQuestionText
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+
+        val messageResId = when{
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+    }
+}
